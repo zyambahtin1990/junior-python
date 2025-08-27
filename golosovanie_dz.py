c14e0cfa045ef005f746972988abf0706c378864
zyambahtin1990 <my_email@domain.ruc>
2025-08-27 22:14:58 +0300

    add file

--- golosovanie_dz.py
+++ golosovanie_dz.py
@@ -1,9 +1,9 @@
-
-
 age = int(input("Введите Ваш возраст: \n"))
 nation_rf = input("Есть ли у Вас гражданство РФ: \n")
 responsibility = input("Привлекались ли Вы к уголовной и иным видам ответсвенности? \n")
-if age >=18 and nation_rf == "да" and responsibility == "нет":
+
+
+if age >= 18 and nation_rf == "да" and responsibility == "нет":
     print("Вы можете участвовать в голосовании")
 else:
     print("Вы не можете участвовать в голосовании")
